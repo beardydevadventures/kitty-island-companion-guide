0b741c40eb87a2954a97d7d0b0297a43e91298c8
Matthew Aisthorpe <m.aisthorpe@hotmail.com>
2025-07-03 16:15:33 +1000

    added basename for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/kitty-island-companion-guide">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
